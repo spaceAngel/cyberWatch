251d75a35515bcfaa45e28b80bc35e503b89d675
spaceAngel <spaceangel@cyberdeck.cz>
2022-05-15 23:39:48 +0200

    UserInterface swipe detection #feature

--- src/UserInterface/UserInterfaceManager.cpp
+++ src/UserInterface/UserInterfaceManager.cpp
@@ -8,6 +8,7 @@
 #include "UserInterface/Screens/SplashScreen.h"
 #include "UserInterface/Screens/ExitScreen.h"
 #include "Utils/TimeUtil.h"
+#include "System/Display.h"
 
 UserInterfaceManager *UserInterfaceManager::_inst;
 
@@ -39,7 +40,51 @@ bool UserInterfaceManager::handleTouch() {
 		TTGOClass::getWatch()->getTouch(x, y)
 		&& (x != 257 && y != 2) //some kind of HW error in my LILLYGO T-Watch (short circuit?)
 	) {
+		if (_swipeEnabled) {
+			_handleSwipe(x);
+		}
 		return true;
 	}
+
+	_stopSwipeHandler();
 	return false;
+}
+
+void UserInterfaceManager::_handleSwipe(uint x) {
+	if (_swipeVector == 0) {
+		if (_swipeLastX == 0) {
+			_swipeLastX = 0 + x;
+		} else {
+			if (_swipeLastX < x) {
+				_swipeVector = VECTOR_RIGHT;
+			 } else {
+				_swipeVector = VECTOR_LEFT;
+			}
+		}
+	} else {
+
+		if (
+			(_swipeVector == VECTOR_RIGHT && _swipeLastX < x)
+			|| (_swipeVector == VECTOR_LEFT &&  _swipeLastX > x)
+		) {
+			_swipeCounter++;
+		} else {
+			_stopSwipeHandler();
+		}
+	}
+	if (_swipeCounter == 2) {
+		Serial.print("swipe \n\n\n");
+		//TO-DO: handle swipe
+	}
+}
+
+void UserInterfaceManager::_stopSwipeHandler() {
+	_swipeCounter = 0;
+	_swipeLastX = 0;
+	_swipeVector = 0;
+	if (Display::getInstance()->isDisplayOn()) {
+		_swipeEnabled = true;
+	} else {
+		_swipeEnabled = false;
+	}
 }
--- src/UserInterface/UserInterfaceManager.h
+++ src/UserInterface/UserInterfaceManager.h
@@ -16,10 +16,22 @@ class UserInterfaceManager {
 
 		protected:
 
+			const uint8_t VECTOR_LEFT = 1;
+			const uint8_t VECTOR_RIGHT = 2;
+			uint16_t _swipeLastX = 0;
+			uint8_t _swipeCounter = 0;
+			int8_t _swipeVector = 0;
+
+			bool _swipeEnabled = false;
+
 			static UserInterfaceManager *_inst;
 
 
 			UserInterfaceManager() {
 			}
+
+			void _stopSwipeHandler();
+
+			void _handleSwipe(uint x);
 
 };
