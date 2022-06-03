e7af803062146ed8037ff9def7476af5288d1201
spaceAngel <spaceangel@cyberdeck.cz>
2022-06-03 20:59:08 +0200

    dateutil  monthNameShort & getDaysinMoth now work with array instaed of big switch #refactor

--- src/Utils/DateUtil.cpp
+++ src/Utils/DateUtil.cpp
@@ -30,41 +30,30 @@ uint8_t DateUtil::weekday(uint year, uint8_t month, uint8_t day) {
 	) % 7;
 }
 
-uint8_t DateUtil::getDaysInMonth(uint year, uint8_t month) {
-	uint8_t rslt;
-	switch (month) {
-		case 1: rslt = 31; break;
-		case 2: rslt = ((year % 4) == 0) ? 29 : 28; break;
-		case 3: rslt = 31; break;
-		case 4: rslt = 30; break;
-		case 5: rslt = 31; break;
-		case 6: rslt = 30; break;
-		case 7: rslt = 31; break;
-		case 8: rslt = 31; break;
-		case 9: rslt = 30; break;
-		case 10: rslt = 31; break;
-		case 11: rslt = 30; break;
-		case 12: rslt = 31; break;
-		default: rslt = 31; break; //fallback for correct code - program flow never reach this point :-)
+uint8_t DateUtil::getDaysInMonth(uint year, int month) {
+	uint8_t rslt = 0;
+	const uint8_t daysInMonth[12] = {
+		31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+	};
+	if ((month >= 1) && (month <= 12)) {
+		if ((month == 2) && ((year % 4) == 0)) {
+			rslt = 29;
+		} else {
+			rslt = daysInMonth[month - 1];
+		}
 	}
 	return rslt;
 }
 
-void DateUtil::monthNameShort(char (&name)[4], uint8_t month) {
-	switch (month)
-	{
-		case 1: (void)strcpy(name, "Jan"); break;
-		case 2: (void)strcpy(name, "Feb"); break;
-		case 3: (void)strcpy(name, "Mar"); break;
-		case 4: (void)strcpy(name, "Apr"); break;
-		case 5: (void)strcpy(name, "May"); break;
-		case 6: (void)strcpy(name, "Jun"); break;
-		case 7: (void)strcpy(name, "Jul"); break;
-		case 8: (void)strcpy(name, "Aug"); break;
-		case 9: (void)strcpy(name, "Sep"); break;
-		case 10: (void)strcpy(name, "Oct"); break;
-		case 11: (void)strcpy(name, "Nov"); break;
-		case 12: (void)strcpy(name, "Dec"); break;
-		default:break; //fallback for correct code - program flow never reach this point :-)
+void DateUtil::monthNameShort(char (&name)[4], int month) {
+	const char * months[12] = {
+		"Jan", "Feb", "Mar",
+		"Apr", "May", "Jun",
+		"Jul", "Aug", "Sep",
+		"Oct", "Nov", "Dec"
+	};
+	if ((month >= 1) && (month <= 12)) {
+		(void)strcpy(name, months[month - 1]);
 	}
+
 }
--- src/Utils/DateUtil.h
+++ src/Utils/DateUtil.h
@@ -9,7 +9,7 @@ class DateUtil {
 	public:
 
 		static uint8_t weekday(uint year, uint8_t month, uint8_t day);
-		static uint8_t getDaysInMonth(uint year, uint8_t month);
-		static void monthNameShort(char (&name)[4], uint8_t month);
+		static uint8_t getDaysInMonth(uint year, int month);
+		static void monthNameShort(char (&name)[4], int month);
 
 };
