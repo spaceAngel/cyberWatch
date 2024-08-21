6e7b12408722b1f13aea9fd107d937511406084f
spaceAngel <spaceangel@cyberdeck.cz>
2024-08-21 23:28:43 +0200

    app menu 20s to lock display #feature

--- src/Apps/AppMenu.h
+++ src/Apps/AppMenu.h
@@ -23,6 +23,10 @@ class AppMenu : public App {
 		virtual bool handleTouch(uint8_t x, uint8_t y);
 		bool handlePEKShort();
 
+		uint16_t getAllowedTimeWithoutActivity() {
+			return 20;
+		}
+
 	protected:
 
 		void renderIcon(const uint16_t *icon, uint8_t x, uint8_t y);
